9bb842db91cf35b76b2c467aed202d1cf15b3a32
EswaraRohan <77735061+EswaraRohan@users.noreply.github.com>
2022-07-13 23:00:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function display(val){
+    document.getElementById('textarea').value +=val;
+}
+
+function calculate(){
+    var val=document.getElementById('textarea').value;
+    var answer=eval(val);
+    document.getElementById('textarea').value=answer;
+}
+
+function clr(){
+    document.getElementById('textarea').value ='';
+}
